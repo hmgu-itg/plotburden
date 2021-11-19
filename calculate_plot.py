59654b4cba09ac0ee5bf3bfffd8c8c5b5e197963
Young-Chan Park <young.chan.park93@gmail.com>
2021-11-19 09:08:48 +0100

    Cleaned up namespaces in calculate_plot.py (This was causing unnecessary problems when trying to read the pickled output)

--- calculate_plot.py
+++ calculate_plot.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 import sys
-import subprocess
-import pandas as pd
-from pandas import notnull, isnull
-import numpy as np
-from numpy import log10, append, nan
-import numpy as np
-import re
-import json, requests, asr
-import urllib.request
-import urllib.parse
-import sys
-import csv
-import seaborn as sns
 import random
 
-#Personal libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import gene_plotter
 import helper_functions
-from helper_functions import *
-from callbacks import *
-from gene_plotter import *
+from helper_functions import info
+
 
 pheno=sys.argv[1]
 gene=sys.argv[2]
@@ -37,16 +28,15 @@ chop=sys.argv[12]
 
 #contdir=sys.argv[13]
 
-import gene_plotter
 gene_plotter.linkedFeatures=linkedFeatures
 helper_functions.contdir=os.path.dirname(__file__)
 # global variables
-server = "https://rest.ensembl.org";
-helper_functions.server=server;
+server = "https://rest.ensembl.org"
+helper_functions.server=server
 
 # Getting Gene coordinates and extend the coordinates with the specified window:
 info("Querying Ensembl for coordinates of "+gene+"...")
-gc=get_coordinates(gene);
+gc = helper_functions.get_coordinates(gene)
 gc.extend(int(window))
 
 #Extract coordinates:
@@ -64,12 +54,12 @@ info("\t\t⇰ Plot boundaries: "+ str(c)+":"+str(start)+"-"+str(end))
 
 ## Getting variant consequences for all variants in the region
 info("Querying Ensembl for SNP consequences and phenotype associations.")
-resp=get_rsid_in_region(gc)
+resp = helper_functions.get_rsid_in_region(gc)
 #resp.to_csv(gene+".snp.data", index=None, sep=",", quoting=csv.QUOTE_NONNUMERIC);
 #resp=pd.read_table("snp.data", sep=",")
 resp['pheno'].replace(to_replace="Annotated by HGMD*", value="", inplace=True, regex=True)
 resp['pheno'].replace(to_replace="ClinVar.*not specified", value="", inplace=True, regex=True)
-resp.loc[isnull(resp.pheno), 'pheno']="none"
+resp.loc[pd.isnull(resp.pheno), 'pheno']="none"
 resp.pheno=resp.pheno.str.strip()
 resp.loc[resp.pheno=="", 'pheno']="none"
 resp['ensembl_rs']=resp['rs']
@@ -81,30 +71,30 @@ info("\t\t⇰ Ensembl provided", len(resp),"known SNPs, ", len(resp[resp.pheno!=
 
 ## Get the single point results. Returns one merged (outer) dataframe with all columns suffixed by the cohort name
 ## We are just going to use this for annotation purposes
-sp = fetch_single_point_meta(gc, sp_results, co_names)
+sp = helper_functions.fetch_single_point_meta(gc, sp_results, co_names)
 info("Read", len(sp), "lines from single-point analysis.");
 sp=pd.merge(sp, resp, on='ps', how='outer')
-sp.loc[isnull(sp.ensembl_rs), 'ensembl_rs']="novel"
-sp.loc[isnull(sp.consequence), 'consequence']="novel"
-sp=sp[notnull(sp.chr)]
+sp.loc[pd.isnull(sp.ensembl_rs), 'ensembl_rs']="novel"
+sp.loc[pd.isnull(sp.consequence), 'consequence']="novel"
+sp=sp[pd.notnull(sp.chr)]
 sp['ensembl_consequence']=sp['consequence']
 sp['chr']=sp['chr'].astype(int)
 info("Getting consequences for novel variants...")
-sp=get_csq_novel_variants(sp, 'chr', 'ps', 'allele0', 'allele1')
+sp = helper_functions.get_csq_novel_variants(sp, 'chr', 'ps', 'allele0', 'allele1')
 
 
 
 ## Get the burden p-values
 ## Returns a dictionary indexed by the cohort name or "meta"
 info("Reading burden P-values...")
-results=read_burden_ps(co_names,smmat_out_file, ensid, pheno, condition_string)
+results = helper_functions.read_burden_ps(co_names,smmat_out_file, ensid, pheno, condition_string)
 # import pickle
 # with open('results.bin', 'rb') as config_dictionary_file:
 # 	results = pickle.load(config_dictionary_file)
 
 ## read all variants in the gene sets including those not in some cohorts
 info("Reading variants from gene set...")
-variants=read_variants_from_gene_set_SMMAT(ensid, condition_string, smmat_set_file)
+variants = helper_functions.read_variants_from_gene_set_SMMAT(ensid, condition_string, smmat_set_file)
 info("Read ", variants.count()[0], "variants in burden across all cohorts")
 ## Now for the plot data
 ##
@@ -114,13 +104,13 @@ i=0
 import pickle
 for n in co_names.split(","):
 	info("Preparing plot data for cohort "+n)
-	rawdat, ld=produce_single_cohort_df(gc, sp_results.split(',')[i], resp, vcf.split(",")[i], smmat_out_file.split(',')[i], smmat_set_file, pheno, condition_string, n, sp, variants)
+	rawdat, ld=helper_functions.produce_single_cohort_df(gc, sp_results.split(',')[i], resp, vcf.split(",")[i], smmat_out_file.split(',')[i], smmat_set_file, pheno, condition_string, n, sp, variants)
 	cohdat[i]=rawdat
 	lddat[i]=ld
 #	cohdat[i]=ColumnDataSource(data=dict(ps=rawdat.ps, logsp=-log10(rawdat.p_score), radii=rawdat.radii, alpha=rawdat.alpha, color=rawdat.color, mafcolor=rawdat.mafcolor, weightcolor=rawdat.weightcolor, outcol=rawdat.outcolor, outalpha=rawdat.outalpha, alpha_prevsig=rawdat.alpha_prevsig, snpid=rawdat.rs, rs=rawdat.ensembl_rs, maf=rawdat.maf, csq=rawdat.ensembl_consequence))
 	i=i+1
 
-cohdat[i], lddat[i] = produce_meta_df(gc, sp, variants, vcf, co_names)
+cohdat[i], lddat[i] = helper_functions.produce_meta_df(gc, sp, variants, vcf, co_names)
 with open('sp.bin', 'wb') as config_dictionary_file:
 	pickle.dump(sp, config_dictionary_file)
 
@@ -244,8 +234,8 @@ sp.dropna(subset=['chr'], inplace=True)
 ## we modified the whole code to have logp calculated within a helper Function
 ## burden logp very unklikely to be that low so we leave logp calculation as is
 rawdat=rawdats[0]
-if -log10(min(results.values()))>maxlogp:
-	maxlogp=-log10(min(results.values()))
+if -np.log10(min(results.values()))>maxlogp:
+	maxlogp=-np.log10(min(results.values()))
 
 import pickle
 plotdat=dict(rawdats=rawdats, rawdat=rawdat, maxlogp=maxlogp, gene=gene, gc=gc, resp=resp, lddat=lddat, sp=sp, cohdat=cohdat, co_split=co_split, results=results, bigdf=bigdf, window=window, chop=chop, pheno=pheno, condition_string=condition_string, linkedFeatures=linkedFeatures)
