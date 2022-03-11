b02882ed85db819edd9882702ba9a838391c5ab1
Young-Chan Park <young.chan.park93@gmail.com>
2022-03-11 20:06:13 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import re
+
+from setuptools import setup, find_packages
+
+
+with open("plotburden/__init__.py") as f:
+    version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
+
+setup(
+    name = "plotburden",
+    version = version,
+    install_requires = [
+        'click',
+        'numpy',
+        'pandas',
+        'bokeh',
+        'requests',
+        'seaborn'
+    ],
+    include_package_data = True,
+    # entry_points = {
+    #     'console_scripts': [
+    #         'peakplotter = peakplotter.main:cli',
+    #         'peakplotter-region = peakplotter.main:cli_region',
+    #         'peakplotter-data-setup = peakplotter._data:setup_data',
+    #     ],
+    # },
+# Metadata
+    author = "Arthur Gilly",
+    author_email = "arthur.gilly@helmholtz-muenchen.de",
+    maintainer = 'Young-Chan Park',
+    maintainer_email = 'young-chan.park@helmholtz-muenchen.de',
+    packages = find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
